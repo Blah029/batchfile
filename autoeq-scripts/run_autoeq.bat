4a23147f3213359072fefc2e84b4f8c69d0637a6
Blah029 <81973454+Blah029@users.noreply.github.com>
2023-02-13 12:28:59 +0530

    Update run_autoeq.bat

--- autoeq-scripts/run_autoeq.bat
+++ autoeq-scripts/run_autoeq.bat
@@ -1,4 +1,4 @@
-cd "D:\User Files\Documents\GitHub\autoeq"
+cd "D:\User Files\Documents\GitHub\autoeq-workspace"
 
 set "MODEL=ASUS TUF FX504"
 set "COMPENSATION=zero"
@@ -9,6 +9,6 @@ CALL "venv\Scripts\activate.bat"
 
 python -m autoeq --input-dir="my_data\%MODEL%" --output-dir="my_results\%MODEL%" --compensation="compensation\%COMPENSATION%.csv" --ten-band-eq --convolution-eq --fs=%SAMPLERATE% --bass-boost=%BASSBOOST% --standardize-input
 
-python -m autoeq --input-dir="my_data\%MODEL%" --output-dir="my_results\%MODEL%\%MODEL% 7BandEQ" --compensation="compensation\%COMPENSATION%.csv"--fixed-band-eq --fixed-band-eq-config=f7beq.yaml --bass-boost=%BASSBOOST% --standardize-input
+python -m autoeq --input-dir="my_data\%MODEL%" --output-dir="my_results\%MODEL%\%MODEL% 7BandEQ" --compensation="compensation\%COMPENSATION%.csv" --fixed-band-eq --fixed-band-eq-config=f7beq.yaml --bass-boost=%BASSBOOST% --standardize-input
 
 python -m autoeq --input-dir="my_data\%MODEL%" --output-dir="my_results\%MODEL%\%MODEL% 9BandEQ" --compensation="compensation\%COMPENSATION%.csv" --fixed-band-eq --fixed-band-eq-config=f9beq.yaml --bass-boost=%BASSBOOST% --standardize-input
