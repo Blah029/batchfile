a37c801091f0bc1b86b5d09f6e1a654ff20a67ec
Blah029 <81973454+Blah029@users.noreply.github.com>
2023-04-15 13:55:21 +0530

    Create run_realsr-ncnn-vulkan.bat

---
+++ realsr-ncnn-vulkan-scripts/run_realsr-ncnn-vulkan.bat
@@ -0,0 +1,22 @@
+cd "D:\Program Files\CLI Programs\realsr-ncnn-vulkan"
+
+set "INPATH=C:\Users\ASUS\Desktop"
+set "INFILE=home02_cropped.png"
+set "OUTPATH=C:\Users\ASUS\Desktop"
+set "SCALE=4"
+set "MODEL=models-DF2K_JPEG"
+
+realsr-ncnn-vulkan.exe -v -i "%INPATH%\%INFILE%" -o "%OUTPATH%\%INFILE%_realsr_n%NOISELEVEL%_%SCALE%x_%MODEL%.png" -s %SCALE%
+pause
+
+:: -h                   show this help
+:: -v                   verbose output
+:: -i input-path        input image path (jpg/png/webp) or directory
+:: -o output-path       output image path (jpg/png/webp) or directory
+:: -s scale             upscale ratio (4, default=4)
+:: -t tile-size         tile size (>=32/0=auto, default=0) can be 0,0,0 for multi-gpu
+:: -m model-path        realsr model path (default=models-DF2K_JPEG)
+:: -g gpu-id            gpu device to use (-1=cpu, default=auto) can be 0,1,2 for multi-gpu
+:: -j load:proc:save    thread count for load/proc/save (default=1:2:2) can be 1:2,2,2:2 for multi-gpu
+:: -x                   enable tta mode
+:: -f format            output image format (jpg/png/webp, default=ext/png)
