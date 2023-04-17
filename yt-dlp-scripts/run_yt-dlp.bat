837814518feacda522c3dcbcf4af1d1c2a93969d
Blah029 <81973454+Blah029@users.noreply.github.com>
2023-04-17 23:39:36 +0530

    Update yt-dlp
    - Add playlist download script
    - Add downloads to gitignore

--- yt-dlp-scripts/run_yt-dlp.bat
+++ yt-dlp-scripts/run_yt-dlp.bat
@@ -1,18 +1,25 @@
 cd "D:\Program Files\CLI Programs\yt-dlp"
 
-set "URL=https://www.youtube.com/watch?v=DmaGYyjDzTM"
+set "URL=https://youtube.com/playlist?list=PLqa1Xuu6Nlo-KCXJP8FIy6sagDobLg3DR"
 set "FORMATCODE="
 
-:: list formats
+:: [list formats]
+:: --------------
 :: yt-dlp -F %URL%
 :: set /p FORMATCODE="Format-code >>> "
 
-:: download format
+:: [download format]
+:: -----------------
 :: yt-dlp -f %FORMATCODE% %URL% -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(title)s.%%(ext)s"
 :: yt-dlp -f %FORMATCODE% -x --audio-format mp3 %URL% -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(title)s.%%(ext)s"
 
-:: download highest quality audio
+:: [download highest quality audio]
+:: --------------------------------
 :: yt-dlp -f bestaudio %URL% -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(title)s.%%(ext)s"
-yt-dlp -f bestaudio -x --audio-format mp3 %URL% -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(title)s.%%(ext)s"
+:: yt-dlp -f bestaudio -x --audio-format mp3 %URL% -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(title)s.%%(ext)s"
+
+:: [download mp3 playlist with metadata]
+:: -------------------------------------
+yt-dlp -f bestaudio -x --audio-format mp3 %URL% --embed-thumbnail --embed-metadata --embed-chapters -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(playlist_index)s %%(title)s.%%(ext)s"
 
 pause
