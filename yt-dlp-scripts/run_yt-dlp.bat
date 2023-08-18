793ca4c67fe2d739d461fdf1c0e0df1fb7224203
Blah029 <81973454+Blah029@users.noreply.github.com>
2023-08-18 18:01:01 +0530

    Fix download path

--- yt-dlp-scripts/run_yt-dlp.bat
+++ yt-dlp-scripts/run_yt-dlp.bat
@@ -1,7 +1,8 @@
 setlocal
-cd "D:\Program Files\CLI Programs\yt-dlp"
+cd "C:\Users\User Programs\CLI Programs\yt-dlp"
 
-set "URL=https://youtube.com/playlist?list=PLqa1Xuu6Nlo-KCXJP8FIy6sagDobLg3DR"
+set "OUTPATH=C:\Users\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads"
+set "URL=https://www.youtube.com/watch?v=x2DXE7Hj4mY"
 set "FORMATCODE="
 
 :: [list formats]
@@ -11,17 +12,17 @@ set "FORMATCODE="
 
 :: [download format]
 :: -----------------
-:: yt-dlp -f %FORMATCODE% %URL% -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(title)s.%%(ext)s"
-:: yt-dlp -f %FORMATCODE% -x --audio-format mp3 %URL% -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(title)s.%%(ext)s"
+:: yt-dlp -f %FORMATCODE% %URL% -o "%OUTPATH%\%%(title)s.%%(ext)s"
+:: yt-dlp -f %FORMATCODE% -x --audio-format mp3 %URL% -o "%OUTPATH%\%%(title)s.%%(ext)s"
 
 :: [download highest quality audio]
 :: --------------------------------
-:: yt-dlp -f bestaudio -x --audio-format mp3 %URL% -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(title)s.%%(ext)s"
+yt-dlp -f bestaudio -x --audio-format mp3 %URL% -o "%OUTPATH%\%%(title)s.%%(ext)s"
 
 :: [download mp3 playlist with metadata]
 :: -------------------------------------
-:: yt-dlp -f bestaudio -x --audio-format mp3 %URL% --embed-thumbnail --embed-metadata --embed-chapters -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(playlist_index)s %%(title)s.%%(ext)s"
-yt-dlp -f bestaudio -x --audio-format mp3 %URL% --embed-thumbnail --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" --embed-metadata --embed-chapters -o "D:\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads\%%(playlist_index)s %%(title)s.%%(ext)s"
+:: yt-dlp -f bestaudio -x --audio-format mp3 %URL% --embed-thumbnail --embed-metadata --embed-chapters -o "%OUTPATH%\%%(playlist_index)s %%(title)s.%%(ext)s"
+:: yt-dlp -f bestaudio -x --audio-format mp3 %URL% --embed-thumbnail --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" --embed-metadata --embed-chapters -o "%OUTPATH%\%%(playlist_index)s %%(title)s.%%(ext)s"
 
 pause
 endlocal
