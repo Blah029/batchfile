d8776eabb0b870ea1931e2e1b021628ae28b71a8
Blah029 <81973454+Blah029@users.noreply.github.com>
2023-11-10 23:29:45 +0530

    Update run_yt-dlp.bat
    - add --break-on-existing
    - erarrange args

--- yt-dlp-scripts/run_yt-dlp.bat
+++ yt-dlp-scripts/run_yt-dlp.bat
@@ -2,7 +2,7 @@ setlocal
 cd "C:\Users\User Programs\CLI Programs\yt-dlp"
 
 set "OUTPATH=C:\Users\User Files\Documents\GitHub\batchfile\yt-dlp-scripts\downloads"
-set "URL=https://www.youtube.com/watch?v=x2DXE7Hj4mY"
+set "URL=https://www.youtube.com/playlist?list=PLqa1Xuu6Nlo-KCXJP8FIy6sagDobLg3DR"
 set "FORMATCODE="
 
 :: [list formats]
@@ -12,17 +12,17 @@ set "FORMATCODE="
 
 :: [download format]
 :: -----------------
-:: yt-dlp -f %FORMATCODE% %URL% -o "%OUTPATH%\%%(title)s.%%(ext)s"
-:: yt-dlp -f %FORMATCODE% -x --audio-format mp3 %URL% -o "%OUTPATH%\%%(title)s.%%(ext)s"
+:: yt-dlp %URL% -f %FORMATCODE% -o "%OUTPATH%\%%(title)s.%%(ext)s"
+:: yt-dlp %URL% -f %FORMATCODE% -x --audio-format mp3 -o "%OUTPATH%\%%(title)s.%%(ext)s"
 
 :: [download highest quality audio]
 :: --------------------------------
-yt-dlp -f bestaudio -x --audio-format mp3 %URL% -o "%OUTPATH%\%%(title)s.%%(ext)s"
+:: yt-dlp %URL% -f bestaudio -x --audio-format mp3 -o "%OUTPATH%\%%(title)s.%%(ext)s"
 
 :: [download mp3 playlist with metadata]
 :: -------------------------------------
-:: yt-dlp -f bestaudio -x --audio-format mp3 %URL% --embed-thumbnail --embed-metadata --embed-chapters -o "%OUTPATH%\%%(playlist_index)s %%(title)s.%%(ext)s"
-:: yt-dlp -f bestaudio -x --audio-format mp3 %URL% --embed-thumbnail --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" --embed-metadata --embed-chapters -o "%OUTPATH%\%%(playlist_index)s %%(title)s.%%(ext)s"
+:: yt-dlp %URL% -f bestaudio -x --audio-format mp3 --embed-thumbnail --embed-metadata --embed-chapters -o "%OUTPATH%\%%(playlist_index)s %%(title)s.%%(ext)s"
+yt-dlp %URL% -f bestaudio -x --audio-format mp3 --embed-thumbnail --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" --embed-metadata --embed-chapters --break-on-existing -o "%OUTPATH%\%%(playlist_index)s %%(title)s.%%(ext)s"
 
 pause
 endlocal
