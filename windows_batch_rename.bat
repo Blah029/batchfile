b29186e359b2fe38a579ef269a7bbde6c91045bb
Blah029 <81973454+Blah029@users.noreply.github.com>
2024-02-16 10:10:30 +0530

    Remove duplicate batchfile

--- windows_batch_rename.bat
+++
@@ -1,9 +0,0 @@
-setlocal
-cd "C:\Users\User Files\Documents\University\Misc\4th Year Work\Final Year Project\Datasets\image-footage\CULane Selected\labels\raw\New folder"
-
-set "PREFIX=driver100_0511_"
-
-echo off
-for %%a in (*.*) do ren "%%a" "%PREFIX%%%a"
-
-endlocal
