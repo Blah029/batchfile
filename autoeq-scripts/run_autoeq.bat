2be4ea39347830575ed5fd8fab82b226126629b9
Blah029 <81973454+Blah029@users.noreply.github.com>
2025-03-13 17:52:02 +0530

    Update JBL BT720 preference adjustment

--- autoeq-scripts/run_autoeq.bat
+++ autoeq-scripts/run_autoeq.bat
@@ -4,14 +4,14 @@ cd "C:\Users\User\User Files\Documents\GitHub\autoeq-workspace"
 set "MODEL=JBL Tune 720BT (Filk)"
 set "COMPENSATION=harman_over-ear_2018_wo_bass"
 set "PREFERENCE=zero"
-set "BASSBOOST=-3,60,0.7"
-set "TREBLEBOOST=3,4000,0.7"
+set "BASSBOOST=-2,60,0.7"
+set "TREBLEBOOST=2,600,0.7"
 set "TILT=0"
 set "SAMPLERATE=44100,48000"
 
 CALL "venv\Scripts\activate.bat"
 
-python -m autoeq --input-dir="my_data\%MODEL%" --output-dir="my_results\%MODEL%" --compensation="compensation\%COMPENSATION%.csv" --sound-signature="my_preference\%PREFERENCE%.csv" --ten-band-eq --convolution-eq --fs=%SAMPLERATE% --bass-boost=%BASSBOOST% --treble-boost=%TREBLEBOOST% --tilt=%TILT% --standardize-input
+python -m autoeq --input-dir="my_data\%MODEL%" --output-dir="my_results\%MODEL%" --compensation="compensation\%COMPENSATION%.csv" --sound-signature="my_preference\%PREFERENCE%.csv" --ten-band-eq --convolution-eq --fs=%SAMPLERATE% --parametric-eq --parametric-eq-config=peq.yaml --bass-boost=%BASSBOOST% --treble-boost=%TREBLEBOOST% --tilt=%TILT% --standardize-input
 
 python -m autoeq --input-dir="my_data\%MODEL%" --output-dir="my_results\%MODEL%\%MODEL% 7BandEQ" --compensation="compensation\%COMPENSATION%.csv" --sound-signature="my_preference\%PREFERENCE%.csv" --fixed-band-eq --fixed-band-eq-config=f7beq.yaml --bass-boost=%BASSBOOST% --treble-boost=%TREBLEBOOST% --tilt=%TILT% --standardize-input
 
--- git-scripts/clone_shallow.bat
+++ git-scripts/clone_shallow.bat
@@ -1,8 +1,13 @@
 setlocal
-cd "D:\User Files\Documents\GitHub"
+cd "C:\Users\User\User Files\Documents\GitHub"
 
+<<<<<<< Updated upstream
 set "URL=https://github.com/goatfungus/NMSSaveEditor.git"
 set "BRANCH=master"
+=======
+set "URL=https://github.com/Blah029/python"
+set "BRANCH=main"
+>>>>>>> Stashed changes
 set "DEPTH=1"
 
 git clone --depth %DEPTH% -b %BRANCH% %URL%
