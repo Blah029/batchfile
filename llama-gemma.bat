3c91a62cd9475b1414320524efa0009779de1fb0
Blah029 <81973454+Blah029@users.noreply.github.com>
2026-08-18 01:41:19 +0530

    update files

--- llama-gemma.bat
+++ llama-gemma.bat
@@ -1,23 +1,42 @@
 @echo off
 setlocal
 
-set "MODEL=C:\Users\User Programs\CLI Programs\llm-cpp\models\gemma-4\gemma-4-E4B-it-Q4_0.gguf"
-set "MTP=C:\Users\User Programs\CLI Programs\llm-cpp\models\gemma-4\mtp-gemma-4-E4B-it-Q4_0.gguf"
-set "MMPROJ=C:\Users\User Programs\CLI Programs\llm-cpp\models\gemma-4\mmproj-gemma-4-E4B-it-Q8_0.gguf"
+set "MODEL_DIR=C:\Users\User Programs\CLI Programs\llama-cpp\models\gemma-4\unsloth"
+set "MODEL=%MODEL_DIR%\gemma-4-E4B-it-qat-UD-Q4_K_XL.gguf"
+set "MTP=%MODEL_DIR%\mtp-gemma-4-E4B-it-qat-Q8_0.gguf"
+set "MMPROJ=%MODEL_DIR%\mmproj-gemma-4-E4B-it-F16.gguf"
 set "VERBOSITY=3"
 
 :: Test
 
-llama-server -m "%MODEL%" -md "%MTP%" -mm "%MMPROJ%" --spec-type draft-mtp -t 8 -c 0 -b 1024 -ub 512 -fa on -ctk q8_0 -ctv q8_0 -ngl all -mg 0 -fit on -fitt 1024 -lv %VERBOSITY% -ctkd q8_0 -ctvd q8_0 --temp 0.5 -td 8 -ngld all --host 127.0.0.1 --port 8080
+llama-server ^
+    -m "%MODEL%" ^
+    -md "%MTP%" ^
+    -mm "%MMPROJ%" ^
+    --spec-type draft-mtp ^
+    -t 8 ^
+    -c 131072 ^
+    -b 1024 ^
+    -ub 512 ^
+    -fa on ^
+    -ctk q8_0 ^
+    -ctv q8_0 ^
+    -ngl all ^
+    -mg 0 ^
+    -lv %VERBOSITY% ^
+    -ctkd q8_0 ^
+    -ctvd q8_0 ^
+    --temp 1.0 ^
+    --top-k 64 ^
+    --top-p 0.95 ^
+    -td 8 ^
+    -ngld all ^
+    --jinja ^
+    --host 127.0.0.1 ^
+    --port 8080
 
 :: Min. batch size for full gpu utilisation -b 512 -ub 256
 :: Min. batch size for image input -b 1024 -ub 512 (not viable for 16 gb ram + 6 gb vram)
-
-:: Archive
-
-:: llama-server -m "%MODEL%" -md "%MTP%" --spec-type draft-mtp -t 8 -c 0 -b 2048 -ub 1024 -fa on -ctk q8_0 -ctv q8_0 -ngl all -mg 0 -fit on -fitt 1024 -ctkd q8_0 -ctvd q8_0 --temp 0.5 -ngld all --host 127.0.0.1 --port 8080
-::llama-server -m "%MODEL%" -md "%MTP%" --spec-type draft-mtp --spec-draft-n-max 3 --spec-draft-ngl auto -ngl auto --fit on --fit-target 1024 -fa auto -ctk q8_0 -ctv q8_0 --ctx-size 8192 --context-shift --parallel 1 --jinja --host 127.0.0.1 --port 8080 --props --metrics
-::llama-server -m "%MODEL%" -md "%MTP%" --spec-type  draft-mtp -c 8192 -ngl all -fa on -t 8 -b 2048 -ub 512 --host 127.0.0.1 --port 8080
 
 @echo off
 endlocal
--- llama-nomic.bat
+++ llama-nomic.bat
@@ -1,9 +1,19 @@
 @echo off
 setlocal
 
-set "MODEL=C:\Users\User Programs\CLI Programs\llm-cpp\models\nomic-embed-text\nomic-embed-text-v1.5.Q8_0.gguf"
+set "MODEL=C:\Users\User Programs\CLI Programs\llama-cpp\models\nomic-embed-text\nomic-embed-text-v1.5.Q8_0.gguf"
 
-llama-server --embedding -ngl all -m "%MODEL%" -c 2048 -b 2048 -ub 2048 --rope-scaling yarn --rope-freq-scale .75 --host 127.0.0.1 --port 8081
+llama-server ^
+    --embedding ^
+    -ngl all ^
+    -m "%MODEL%" ^
+    -c 2048 ^
+    -b 2048 ^
+    -ub 2048 ^
+    --rope-scaling yarn ^
+    --rope-freq-scale .75 ^
+    --host 127.0.0.1 ^
+    --port 8081
 
 @echo off
 endlocal
